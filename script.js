856c17b02384596f36d1b2127988b69bab0d89ff
Thamonwan <thamonwan_as@cmu.ac.th>
2021-06-23 14:08:48 +0700

    rename
    "favorite" to "my list"

---
+++ script.js
@@ -0,0 +1,9 @@
+function mouseClick() {
+    var txt;
+    if (confirm("You need to save on My list")) {
+        txt = " "
+    }else {
+        txt = " "
+    }
+    document.getElementById("myP").innerHTML = txt;
+}
